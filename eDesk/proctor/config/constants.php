99789456386ca74c787efe84cc8aac09c79fe528
Md. Waliul Islam Rayhan <b190305034@cse.jnu.ac.bd>
2023-12-08 22:15:12 +0600

    Remove File

--- eDesk/proctor/config/constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-define('ROOT_URL', 'http://localhost/eDesk/');
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'rayhan');
-define('DB_PASS', 'admin123');
-define('DB_NAME', 'edeskdb');
